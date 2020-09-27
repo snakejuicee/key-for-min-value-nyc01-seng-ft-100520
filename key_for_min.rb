0937589f6ddab1448346ba31dfb36ce78f9edf78
Patricia Arnedo <paaarnedo@gmail.com>
2020-09-27 02:20:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,7 @@ def key_for_min_value(name_hash)
     return nil
   end
   
-  min = name_hash.default
-  result = name_hash[0]
+  min = 1000
   
   name_hash.each do |key, value|
     if value < min
